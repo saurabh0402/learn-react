426439a984a0211ce9322cc11c88b6d2277bc366
saurabh0402 <saurabh.k0402@gmail.com>
2018-06-23 09:36:38 +0530

    Initial Setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	entry: __dirname + "/src/js/index.js",
+	mode: "development",
+	output: {
+		path: __dirname + "/dist/assets",
+		filename: "build.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: [
+					{
+						loader: 'babel-loader',
+						query: {
+							presets: ['env', 'stage-0', 'react']
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				exclude: /(node_modules)/,
+				loader: ['style-loader', 'css-loader']
+			}
+		]	
+	},
+	plugins: [
+		new UglifyJsPlugin()
+	]
+}
