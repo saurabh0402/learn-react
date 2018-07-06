c3392b1e3088d419c372fa7f80faa0b64ce98e90
saurabh0402 <saurabh.k0402@gmail.com>
2018-07-06 08:02:54 +0530

    Add new app

---
+++ progressive-weather-app/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/main.css';
+import './css/all.css';
+import App from './components/app';
+
+render(<App />, document.getElementById('react-container'));
