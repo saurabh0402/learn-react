7326666d470de14931ddd97e7bcbdc3bb1622574
saurabh0402 <saurabh.k0402@gmail.com>
2018-07-09 12:50:56 +0530

    Load weather on city addition

--- progressive-weather-app/src/components/app.jsx
+++ progressive-weather-app/src/components/app.jsx
@@ -13,7 +13,8 @@ class App extends React.Component {
 		this._weatherApiKey = config.weatherId;
 		this.state = {
 			curWeather: [],
-			showForm: false
+			showForm: false,
+			error: false
 		};
 
 		this.toggleFrom = this.toggleFrom.bind(this);
@@ -21,44 +22,84 @@ class App extends React.Component {
 	}
 
 	toggleFrom(){
-		console.log(this.state)
+		//console.log(this.state)
 		this.setState({
 			showForm: !this.state.showForm
 		});
 	}
 
-	componentDidMount(){}
+	componentDidMount(){
+		console.log("In!!");
+	}
 
 	addCity(city){
-		let t = {
-			id: city.Key,
-			placeDetails: {
-				name: city.LocalizedName,
-				country: city.Country.LocalizedName,
-				administrativeArea: city.AdministrativeArea.LocalizedName
-			},
-			weatherDetails: {
-				temp: null,
-				text: '',
-				icon: ''
-			},
-			lastUpdated: new Date(null)
-		};
+		this.getWeather(city.Key).then((weather) => {
+			//console.log(weather)
 
-		let curWeather = this.state.curWeather;
-		curWeather.push(t);
+			weather = weather[0];
+			let icon = "https://developer.accuweather.com/sites/default/files/";
+			if(weather.WeatherIcon < 10)
+				icon += "0";
+			icon += weather.WeatherIcon + "-s.png"
+			let t = {
+				id: city.Key,
+				placeDetails: {
+					name: city.LocalizedName,
+					country: city.Country.LocalizedName,
+					administrativeArea: city.AdministrativeArea.LocalizedName
+				},
+				weatherDetails: {
+					temp: weather.Temperature.Metric.Value,
+					text: weather.WeatherText,
+					icon: icon
+				},
+				lastUpdated: new Date(null)
+			};
 
-		this.setState({
-			showForm: false,
-			curWeather: curWeather
+			let curWeather = this.state.curWeather;
+			curWeather.push(t);
+
+			this.setState({
+				showForm: false,
+				curWeather: curWeather
+			});
+		}).catch(e => {
+			this.setState({
+				error: true
+			})
 		});
+	}
+
+	getWeather(city=""){
+		if(city === ""){
+
+		} else {
+			return new Promise(function(resolve, reject){
+				console.log(city);
+				fetch(`http://dataservice.accuweather.com/currentconditions/v1/${city}?apikey=${config.weatherId}`)
+					.then(res => res.json())
+					.then(res => {
+						resolve(res)
+					})
+					.catch(e => reject(e))
+			});
+		}
 	}
 
 	render(){
 		return (
 			<div className="cont">
-				{ this.state.showForm ?
-					<Form toggleForm={this.toggleFrom} addCity={this.addCity} /> : <Home toggleForm={this.toggleFrom} cities={this.state.curWeather} />
+				{ this.state.showForm &&
+					<Form toggleForm={this.toggleFrom} addCity={this.addCity} />
+				}
+				{ !this.state.showForm && !this.error &&
+					<Home toggleForm={this.toggleFrom} cities={this.state.curWeather} />
+				}
+				{ !this.state.showForm && this.error &&
+					<div className="show-message">
+						<i className="fa fa-frown" />
+						<div className="message"> Error Fetching weather! </div>
+					</div>
 				}
 			</div>
 		)
--- progressive-weather-app/src/components/weather/weather.jsx
+++ progressive-weather-app/src/components/weather/weather.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Weather = ({city}) => {
 	return (
-		<h1> {city.placeDetails.name} </h1>
+		<div className="weather">
+			<h1> {`${city.placeDetails.name}, ${city.placeDetails.administrativeArea}, ${city.placeDetails.country}`} </h1>
+			<h1> {city.weatherDetails.temp} <sup> o </sup> C </h1>
+			<img src={city.weatherDetails.icon} />
+			<h1> {city.weatherDetails.text} </h1>
+		</div>
 	)
 };
 
