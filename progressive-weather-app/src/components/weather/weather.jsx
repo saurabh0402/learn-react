2f0a20640a271685d3cc58884506777d34dc0992
saurabh0402 <saurabh.k0402@gmail.com>
2018-07-21 10:31:43 +0530

    Design weather details

--- progressive-weather-app/src/components/weather/weather.jsx
+++ progressive-weather-app/src/components/weather/weather.jsx
@@ -3,10 +3,24 @@ import React from 'react';
 const Weather = ({city}) => {
 	return (
 		<div className="weather">
-			<h1> {`${city.placeDetails.name}, ${city.placeDetails.administrativeArea}, ${city.placeDetails.country}`} </h1>
-			<h1> {city.weatherDetails.temp} <sup> o </sup> C </h1>
-			<img src={city.weatherDetails.icon} />
-			<h1> {city.weatherDetails.text} </h1>
+			<div className="left">
+				<div className="city-name"> {`${city.placeDetails.name}, ${city.placeDetails.administrativeArea}, ${city.placeDetails.country}`} </div>
+				<div className="temp"> {city.weatherDetails.temp} <sup> o </sup> C </div>
+			</div>
+			<div className="right">
+				<div className="weather-image">
+					<img src={city.weatherDetails.icon} />
+				</div>
+				<div className="weather-details"> {city.weatherDetails.text} </div>
+			</div>
+			<div className="button-cont">
+				<button class="refresh-button">
+					<i class="fas fa-redo-alt"></i>
+				</button>
+				<button class="remove-button">
+					<i class="fas fa-times"></i>
+				</button>
+			</div>
 		</div>
 	)
 };
